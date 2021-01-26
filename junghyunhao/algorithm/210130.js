ed97ff1947d611768d9b1995f90dece354871b74
JUNG HYUN KIM <junghyunhao@gmail.com>
2021-01-26 23:47:24 +0900

    [210125] algorithm : add algorithm question  for this week

--- junghyunhao/algorithm/210130.js
+++ junghyunhao/algorithm/210130.js
@@ -1,9 +1,15 @@
 /**
  * 완주하지 못한 선수
  https://programmers.co.kr/learn/courses/30/lessons/42576
-
+ 블로그 포스팅
+ 
  */
 function solution(participant, completion) {
-  var answer = "";
-  return answer;
+  for (let i = 0; i < completion.length; i++) {
+    let repetitiveIndex = participant.indexOf(completion[i]);
+    participant.splice(repetitiveIndex, 1);
+  }
+  return participant[0];
 }
+
+solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]);
