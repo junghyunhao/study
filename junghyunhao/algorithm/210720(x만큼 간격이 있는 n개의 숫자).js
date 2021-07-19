e2a001c5d9f4eea0e80ed71cc2c73ef6802bd153
junghyunhao <junghyunhao@gmail.com>
2021-07-20 08:22:44 +0900

    [210720] algorithm : n.length of array with x times apart

--- junghyunhao/algorithm/210720(x만큼 간격이 있는 n개의 숫자).js
+++ junghyunhao/algorithm/210720(x만큼 간격이 있는 n개의 숫자).js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {*} x
+ * @param {*} n
+ * https://programmers.co.kr/learn/courses/30/lessons/12954
+ */
 function solution(x, n) {
   let answer = [];
   for (let i = 1; i <= n; i++) {
